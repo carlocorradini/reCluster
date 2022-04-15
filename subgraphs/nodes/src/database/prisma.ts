48f1cde94ab25d82441fa237a27b4472603b2ff8
Carlo <carlo.ita98@gmail.com>
2022-04-15 18:30:33 +0200

    refactor: database url when creating prismaclient

--- subgraphs/nodes/src/database/prisma.ts
+++ subgraphs/nodes/src/database/prisma.ts
@@ -23,5 +23,8 @@
  */
 
 import { PrismaClient } from '@prisma/client';
+import { config } from '~/config';
 
-export const prisma = new PrismaClient();
+export const prisma = new PrismaClient({
+  datasources: { db: { url: config.database.url } }
+});
