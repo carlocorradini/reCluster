d6b55d113cb50552ebd4111303853a0cce215996
Carlo <carlo.ita98@gmail.com>
2022-04-16 21:33:13 +0200

    chore: trying custom alpine linux image

--- linux/mkimg.recluster.sh
+++ linux/mkimg.recluster.sh
@@ -22,17 +22,16 @@
 # SOFTWARE.
 
 profile_recluster() {
-        profile_standard
-        kernel_cmdline=""
-        syslinux_serial="0 115200"
-        kernel_addons="zfs spl"
-        apks="$apks "
-        local _k _a
-        for _k in $kernel_flavors; do
-          apks="$apks linux-$_k"
-          for _a in $kernel_addons; do
-            apks="$apks $_a-$_k"
-          done
+    profile_standard
+    kernel_cmdline="unionfs_size=512M console=tty0 console=ttyS0,115200"
+    syslinux_serial="0 115200"
+    kernel_addons="zfs"
+    apks="$apks iscsi-scst zfs-scripts zfs zfs-utils-py cciss_vol_status lvm2 mdadm mkinitfs mtools nfs-utils parted rsync sfdisk syslinux unrar util-linux xfsprogs dosfstools ntfs-3g"
+    for _kf in $kernel_flavors; do
+        apks="$apks linux-$_kf"
+        for _ka in $kernel_addons; do
+            apks="$apks $_ka-$_kf"
         done
-        apks="$apks linux-firmware"
+    done
+    apks="$apks linux-firmware"
 }
