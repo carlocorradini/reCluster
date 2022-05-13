d7e14552740238d65b534c572d5423f3a98cf383
Carlo <carlo.ita98@gmail.com>
2022-05-13 19:43:18 +0200

    chore(router): updated router in script

--- backend/scripts/router.sh
+++ backend/scripts/router.sh
@@ -25,7 +25,7 @@
 DIRNAME="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 readonly DIRNAME
 # Apollo Router version
-readonly APOLLO_ROUTER_VERSION=v0.9.0-rc.0
+readonly APOLLO_ROUTER_VERSION=v0.9.0
 # Apollo Router image
 readonly APOLLO_ROUTER_IMAGE="ghcr.io/apollographql/router:$APOLLO_ROUTER_VERSION"
 # Apollo Router config
