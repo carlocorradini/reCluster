5508a5eb656c213eff4a25e8a55858ed4c10ed7a
Carlo <carlo.ita98@gmail.com>
2022-10-21 13:45:32 +0200

    fix: use config node env

--- server/src/logger/logger.ts
+++ server/src/logger/logger.ts
@@ -27,5 +27,5 @@ import { config } from '~/config';
 
 export const logger = pino({
   name: config.app,
-  level: process.env.NODE_ENV === 'development' ? 'debug' : 'info'
+  level: config.node.env === 'development' ? 'debug' : 'info'
 });
