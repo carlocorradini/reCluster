14c2390893a8616d241bfe704dc2b7d5d6ae5f46
Carlo <carlo.ita98@gmail.com>
2022-10-29 18:49:32 +0200

    feat: save graphql schema to file script

---
+++ server/scripts/schema.ts
@@ -0,0 +1,56 @@
+#!/usr/bin/env ts-node
+
+/*
+ * MIT License
+ *
+ * Copyright (c) 2022-2022 Carlo Corradini
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+import 'reflect-metadata';
+import yargs from 'yargs';
+import pino from 'pino';
+import { lexicographicSortSchema } from 'graphql';
+import { printSchemaWithDirectives } from '@graphql-tools/utils';
+import { outputFileSync } from 'type-graphql/dist/helpers/filesystem';
+import { schema } from '../src/graphql';
+
+const logger = pino({ level: 'debug', name: 'graphql-schema' });
+
+const GRAPHQL_SCHEMA = 'schema.graphql';
+
+// Arguments
+const argv = yargs(process.argv.slice(2))
+  .options({
+    output: {
+      type: 'string',
+      default: GRAPHQL_SCHEMA,
+      description: 'Output path'
+    }
+  })
+  .help()
+  .parseSync();
+
+// Save schema
+logger.info(`Saving GraphQL schema to '${argv.output}'`);
+outputFileSync(
+  argv.output,
+  printSchemaWithDirectives(lexicographicSortSchema(schema))
+);
