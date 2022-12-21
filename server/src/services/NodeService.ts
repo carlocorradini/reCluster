6433c06a7d5bcb603a0ade330718b29454b81e9e
carlocorradini <carlo.ita98@gmail.com>
2022-12-21 14:10:06 +0100

    refactor: boot without address

--- server/src/services/NodeService.ts
+++ server/src/services/NodeService.ts
@@ -327,7 +327,6 @@ export class NodeService {
           select: {
             nodePoolId: true,
             nodePoolAssigned: true,
-            address: true,
             interfaces: {
               where: { wol: { isEmpty: false } },
               select: { address: true }
@@ -346,10 +345,7 @@ export class NodeService {
       // Bootstrap
       await Promise.any(
         node.interfaces.map((intf) =>
-          this.wolService.wake({
-            mac: intf.address,
-            address: node.address
-          })
+          this.wolService.wake({ mac: intf.address })
         )
       );
 
